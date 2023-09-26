9ebdb2f64420b681ee70309dca5fed5596ee38de
toni-akintola <oakintol@nd.edu>
2023-09-26 15:39:09 -0400

    notification v1

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ const {log, logger} = require("firebase-functions/logger");
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { onDocumentCreated, onDocumentUpdated } = require("firebase-functions/v2/firestore");
+const getMessaging = require("firebase-admin/messaging");
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
@@ -99,7 +100,7 @@ const postsPath = "users/{school}/posts/{postID}";
 
 exports.postNotifications = onDocumentUpdated(postsPath, async (event) => {
   functions.logger.log("post document was updated");
-
+  const postID = event.params.postID
   const beforeSnapshot = event.data.before.data();
   const afterSnapshot = event.data.after.data();
   if (!beforeSnapshot || !afterSnapshot) {
@@ -111,43 +112,102 @@ exports.postNotifications = onDocumentUpdated(postsPath, async (event) => {
   const school = event.params.school
   if(afterSnapshot.numLikes > beforeSnapshot.numLikes) {
     functions.logger.log(afterSnapshot.numLikes)
-    functions.logger.log(beforeSnapshot.numLikes)  
-    const usersWhoLiked = afterSnapshot.usersWhoLiked
-    // functions.logger.log(usersWhoLiked) 
-    // const lastLikeUser = usersWhoLiked[usersWhoLiked.length - 1]
-    // functions.logger.log(lastLikeUser)
-    // const lastUserSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(lastLikeUser).get()
-    // const lastUserName = lastUserSnap.data().name
+    functions.logger.log(beforeSnapshot.numLikes) 
+
+    const postSnap = await admin.firestore().collection(`users/${school}/posts`).doc(postID).get()
+    const post = postSnap.data()
+
+    const usersWhoLiked = post.usersWhoLiked
+    functions.logger.log(usersWhoLiked) 
+    const lastLikeUserID = usersWhoLiked[usersWhoLiked.length - 1]
+    functions.logger.log(lastLikeUserID)
+
+    if(beforeSnapshot.numLikes < 10 && afterSnapshot.numLikes >= 10) {
+      functions.logger.log("Fired mass notification")
+      const tokensQuery = await admin.firestore().collection(`notifications/${school}/fcmTokens`).get()
+      const tokens = tokensQuery.docs.map(doc => doc.data().fcmToken)
+      functions.logger.log(tokens)
+      const message = {
+        tokens: tokens,
+        notification: {
+        title: `${posterName}'s post is roasting right now!`,
+        body: `${posterName}'s post just reached ${afterSnapshot.numLikes} likes!`
+      }
+    }
+      functions.logger.log(message)
+      const response = await admin.messaging().sendEachForMulticast(message).then((response) => {
+        functions.logger.log(response.successCount + " messages were sent successfully")
+      })
+    };
+  }
+    if(lastLikeUserID == uid) {
+      return;
+    }
+
+    const lastUserSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(lastLikeUserID).get()
+    const lastUserName = lastUserSnap.data().name
+    functions.logger.log(lastUserName)
+
     const tokenSnap = await admin.firestore().collection(`notifications/${school}/fcmTokens`).doc(uid).get()
     const token = tokenSnap.data().fcmToken
     const response = await admin.messaging().send({
       token: token,
       notification: {
         title: "new like!",
-        body: `someone just liked your post!`
+        body: `${lastUserName} just liked your post!`
       },
     })
     functions.logger.log("successfully sent notification");
-  }
 
-  // if(afterSnapshot.numLikes >= 10) {
-  //   const tokensQuery = await admin.firestore().collection(`notifications/${school}/fcmTokens`).get()
-  //   const tokens = tokensQuery.docs.map(doc => doc.data().fcmToken)
-  //   functions.logger.log(tokens)
-    
-  //   tokens.map(token => (
-  //     response = await admin.messaging().send({
-  //       token: token,
-  //       notification: {
-  //         title: "new friend request!",
-  //         body: `${name} just sent you a friend request!`
-  //       },
-  //     })
-  //   ))
-  // };
-  
+
 });
 
+exports.friendPostNotification = onDocumentCreated(postsPath, async (event) => {
+  functions.logger.log("post document was created")
+
+  const snapshot = event.data;
+  if (!snapshot) {
+      console.log("No data associated with the event");
+      return;
+  }
+  const snapshotData = snapshot.data()
+  const posterID = snapshotData.posterId;
+  const school = event.params.school;
+  const posterName = snapshotData.name;
+
+  const posterRelationshipsSnap = await admin.firestore().collection(`relationships/${school}/relationships`).doc(posterID).get()
+  const friends = posterRelationshipsSnap.data().friends
+  functions.logger.log(friends);
+  const friendIDs = friends.map(friend => (friend.userID))
+  functions.logger.log(friendIDs)
+  const tokens = []
+  for (let index = 0; index < friendIDs.length; index++) {
+    const tokenSnap = await admin.firestore().collection(`notifications/${school}/fcmTokens`).doc(friendIDs[index]).get()
+    if(!tokenSnap) {
+      continue;
+    }
+    const tokenData = tokenSnap.data()
+    if(!tokenData) {
+      continue;
+    }
+    const token = tokenData.fcmToken
+    functions.logger.log(token)
+    tokens.push(token);
+  }
+  
+  const message = {
+    tokens: tokens,
+    notification: {
+    title: `your friend ${posterName} just posted!`,
+    body: `check the new tab to see ${posterName}'s new post!`
+  }
+}
+
+const response = await admin.messaging().sendEachForMulticast(message).then((response) => {
+  functions.logger.log(response.successCount + " messages were sent successfully")
+})
+  
+})
 const commentsPath = "users/{school}/posts/{postID}/comments/{commentID}";
 
 exports.commentNotifications = onDocumentCreated(commentsPath, async (event) => {
@@ -163,12 +223,11 @@ exports.commentNotifications = onDocumentCreated(commentsPath, async (event) => 
   const school = event.params.school
   const post = await admin.firestore().collection(`users/${school}/posts`).doc(postID).get()
   const posterID = post.data().posterId 
-    // functions.logger.log(usersWhoLiked) 
-    // const lastLikeUser = usersWhoLiked[usersWhoLiked.length - 1]
-    // functions.logger.log(lastLikeUser)
-    // const lastUserSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(lastLikeUser).get()
-    // const lastUserName = lastUserSnap.data().name
+
     const commenterID = comment.posterId
+    if(commenterID == posterID) {
+      return;
+    }
     const commentText = comment.comment
     const commentSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(commenterID).get() 
     const commenter = commentSnap.data()
@@ -184,5 +243,79 @@ exports.commentNotifications = onDocumentCreated(commentsPath, async (event) => 
     })
     functions.logger.log("successfully sent notification");
 
-  
 });
+
+const repliesPath = "users/{school}/posts/{postID}/comments/{commentID}/replies/{replyID}"
+exports.repliesNotifications = onDocumentCreated(repliesPath, async (event) => {
+  functions.logger.log("reply document was created");
+
+  const snapshot = event.data;
+  if (!snapshot) {
+      console.log("No data associated with the event");
+      return;
+  }
+  const reply = snapshot.data()
+  const replyText = reply.reply
+
+  const postID = event.params.postID
+  const commentID = event.params.commentID
+  const school = event.params.school
+
+  const commentSnap = await admin.firestore().collection(`users/${school}/posts/${postID}/comments`).doc(commentID).get()
+  const comment = commentSnap.data()
+  const commenterID = comment.posterId
+  const replierSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(reply.posterId).get()
+  const replier = replierSnap.data()
+
+  if (replier.userID == commenterID) {
+    return
+  }
+
+  const name = replier.name
+  const tokenSnap = await admin.firestore().collection(`notifications/${school}/fcmTokens`).doc(comment.posterId).get()
+  const token = tokenSnap.data().fcmToken
+  const response = await admin.messaging().send({
+    token: token,
+    notification: {
+      title: "new reply!",
+      body: `${name} just replied to your comment: "${replyText}"`
+    },
+  })
+  functions.logger.log("successfully sent notification");
+
+});
+
+// exports.commentNotifications = onDocumentUpdated(commentsPath, async (event) => {
+//   functions.logger.log("comment document was created");
+
+//   const snapshot = event.data;
+//   if (!snapshot) {
+//       console.log("No data associated with the event");
+//       return;
+//   }
+//   const comment = snapshot.data()
+//   const postID = event.params.postID
+//   const school = event.params.school
+//   const post = await admin.firestore().collection(`users/${school}/posts`).doc(postID).get()
+//   const posterID = post.data().posterId 
+
+//     const commenterID = comment.posterId
+//     if(commenterID == posterID) {
+//       return;
+//     }
+//     const commentText = comment.comment
+//     const commentSnap = await admin.firestore().collection(`users/${school}/profiles`).doc(commenterID).get() 
+//     const commenter = commentSnap.data()
+//     const name = commenter.name
+//     const tokenSnap = await admin.firestore().collection(`notifications/${school}/fcmTokens`).doc(posterID).get()
+//     const token = tokenSnap.data().fcmToken
+//     const response = await admin.messaging().send({
+//       token: token,
+//       notification: {
+//         title: "new like!",
+//         body: `${name} just liked your comment: "${commentText}"`
+//       },
+//     })
+//     functions.logger.log("successfully sent notification");
+
+// });
